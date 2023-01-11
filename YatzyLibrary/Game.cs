92ae62da75a5060b8ec4c8ee259857ea349e093e
Tommy Mendys <tommy.mendys@gmail.com>
2023-01-11 14:19:41 +0100

    Updated the Game class to .NET 6 standard. simplified the GetWinner method

--- YatzyLibrary/Game.cs
+++ YatzyLibrary/Game.cs
@@ -1,42 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace YatzyLibrary;
 
-namespace YatzyLibrary
+public class Game
 {
-    public class Game
+    /// <summary>
+    /// Checks if the game is over.
+    /// </summary>
+    /// <param name="players">the list of all players.</param>
+    /// <returns>If the game is over or not.</returns>
+    public static bool EndCondition(IEnumerable<Player> players)
     {
-        /// <summary>
-        /// Checks if the game is over.
-        /// </summary>
-        /// <param name="players">the list of all players.</param>
-        /// <returns>If the game is over or not.</returns>
-        public static bool EndCondition(List<Player> players)
+        foreach (Player player in players)
         {
-            foreach (Player player in players)
+            for (int i = 0; i < player.ScoreTable.Length; i++)
             {
-                for (int i = 0; i < player.ScoreTable.Length; i++)
+                if (player.ScoreTable[i] == 0)
                 {
-                    if(player.ScoreTable[i] == 0)
-                    {
-                        return false;
-                    }
+                    return false;
                 }
             }
-
-            return true;
         }
 
-        /// <summary>
-        /// Will get the winner by sorting by score and return the first in the list.
-        /// </summary>
-        /// <param name="players">All players</param>
-        /// <returns>The player with highest score.</returns>
-        public static Player GetWinner(List<Player> players)
-        {
-            players.Sort((x, y) => x.Score.CompareTo(y.Score));
-            return players.First();
-        }
+        return true;
     }
+
+    /// <summary>
+    /// Returns the player with highest score.
+    /// </summary>
+    /// <param name="players">All players</param>
+    /// <returns>The player with highest score.</returns>
+    public static Player GetWinner(IEnumerable<Player> players) =>
+        players.MaxBy(x => x.Score);
 }
