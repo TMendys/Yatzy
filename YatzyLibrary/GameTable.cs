ed401e5962e48e4852905da2e6b9a77e1f60b8a6
Tommy Mendys <tommy.mendys@gmail.com>
2023-01-11 13:58:50 +0100

    Updated GameTable class to .Net 6 standard. Added a few TODO items

--- YatzyLibrary/GameTable.cs
+++ YatzyLibrary/GameTable.cs
@@ -1,101 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace YatzyLibrary;
 
-namespace YatzyLibrary
+public enum YahtzeeCombination
 {
-    public enum YahtzeeCombination
+    Ones = 1,
+    Twos = 2,
+    Threes = 3,
+    Fours = 4,
+    Fives = 5,
+    Sixes = 6,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FourOfAKind,
+    FullHouse,
+    SmallStraight,
+    LongStraight,
+    Chance,
+    Yatzee
+}
+
+public static class GameTable
+{
+    /// <summary>
+    /// All the table names.
+    /// </summary>
+    public static readonly string[] TableNames =
+    { "Ettor", "Tvåor", "Treor", "Fyror", "Femmor", "Sexor", "Summa", "Bonus",
+        "Ett par", "Två par", "Tretal", "Fyrtal", "Kåk", "Liten stege", "Stor stege",
+        "Chans", "Yatzy", "Summa" };
+
+    /// <summary>
+    /// Will put the score into the array (score table).
+    /// </summary>
+    /// <param name="player">The player</param>
+    /// <param name="score">The score</param>
+    /// <param name="column">Where to put the score into the array.</param>
+    public static void InputScore(Player player, int score, int column)
     {
-        Ones = 1,
-        Twos = 2,
-        Threes = 3,
-        Fours = 4,
-        Fives = 5,
-        Sixes = 6,
-        OnePair,
-        TwoPair,
-        ThreeOfAKind,
-        FourOfAKind,
-        FullHouse,
-        SmallStraight,
-        LongStraight,
-        Chance,
-        Yatzee
+        if (score == 0)
+            player.ScoreTable[column] = -1;
+        else
+            player.ScoreTable[column] = score;
+
+        SumAndBonusChecker(player);
     }
 
-    public static class GameTable
+    /// <summary>
+    /// Checks all the sums and if bonus is reach.
+    /// </summary>
+    /// <param name="player">The player</param>
+    private static void SumAndBonusChecker(Player player)
     {
-        /// <summary>
-        /// All the table names.
-        /// </summary>
-        public static readonly string[] TableNames = {"Ettor      ", "Tvåor      ", "Treor      ", "Fyror      ", "Femmor     ", "Sexor      ", "Summa      ", "Bonus      ",
-            "Ett par    ", "Två par    ", "Tretal     ", "Fyrtal     ", "Kåk        ", "Liten stege", "Stor stege ", "Chans      ", "Yatzy      ", "Summa      " };
-        
-        /// <summary>
-        /// Will put the score into the array (score table).
-        /// </summary>
-        /// <param name="player">The player</param>
-        /// <param name="score">The score</param>
-        /// <param name="column">Where to put the score into the array.</param>
-        public static void InputScore(Player player, int score, int column)
-        {
-            if (score == 0)
-                player.ScoreTable[column] = -1;
-            else
-                player.ScoreTable[column] = score;
+        // TODO: Make this method less dependent on index numbers, use variable names instead.
+        int sum = 0;
 
-            SumAndBonusChecker(player);
+        for (int i = 0; i < 6; i++)
+        {
+            sum += player.ScoreTable[i];
+            if (player.ScoreTable[i] == -1)
+                sum += 1;
         }
 
-        /// <summary>
-        /// Checks all the sums and if bonus is reach.
-        /// </summary>
-        /// <param name="player">The player</param>
-        private static void SumAndBonusChecker(Player player)
+        player.ScoreTable[6] = sum;
+
+        if (sum > 62)
         {
-            int sum = 0;
+            player.ScoreTable[7] = 50;
+        }
+        else
+            player.ScoreTable[7] = -1;
 
-            for (int i = 0; i < 6; i++)
-            {
-                sum += player.ScoreTable[i];
-                if (player.ScoreTable[i] == -1)
-                    sum += 1;
-            }
-
-            player.ScoreTable[6] = sum;
-
-            if(sum > 62)
-            {
-                player.ScoreTable[7] = 50;
-            }
-            else
-                player.ScoreTable[7] = -1;
-
-            for (int i = 7; i < player.ScoreTable.Length-1; i++)
-            {
-                sum += player.ScoreTable[i];
-                if (player.ScoreTable[i] == -1)
-                    sum += 1;
-            }
-
-            player.ScoreTable[player.ScoreTable.Length-1] = sum;
+        for (int i = 7; i < player.ScoreTable.Length - 1; i++)
+        {
+            sum += player.ScoreTable[i];
+            if (player.ScoreTable[i] == -1)
+                sum += 1;
         }
 
-        /// <summary>
-        /// Loads the table into an list of arrays.
-        /// </summary>
-        /// <param name="players">All the players</param>
-        /// <returns>A list of arrays containing the score tables of every player.</returns>
-        public static List<int[]> LoadTable(List<Player> players)
+        player.ScoreTable[player.ScoreTable.Length - 1] = sum;
+    }
+
+    /// <summary>
+    /// Loads the table into an list of arrays.
+    /// </summary>
+    /// <param name="players">All the players</param>
+    /// <returns>A list of arrays containing the score tables of every player.</returns>
+    public static IEnumerable<int[]> LoadTable(IEnumerable<Player> players)
+    {
+        List<int[]> scoreTable = new List<int[]>();
+
+        foreach (Player player in players)
         {
-            List<int[]> scoreTable = new List<int[]>();
-
-            foreach (Player player in players)
-            {
-                scoreTable.Add(player.ScoreTable);
-            }
-
-            return scoreTable;
+            // TODO: Change this method to make use of yield
+            scoreTable.Add(player.ScoreTable);
         }
+
+        return scoreTable;
     }
 }
